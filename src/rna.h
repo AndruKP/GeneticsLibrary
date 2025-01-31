f6654c0e77e3287564c665efffef0e592196239c
AndruKP <andruhovski@outlook.com>
2025-01-31 19:18:29 +0100

    RNA tests are finished

--- src/rna.h
+++ src/rna.h
@@ -12,11 +12,30 @@ class DNA;
 
 class RNA : public SequenceWrapper<RNA_VALIDATOR > {
 public:
+    /**
+     *
+     * @param seq
+     * @param dir
+     */
     explicit RNA(const std::string &seq, Directionality dir = Directionality::DIR_3_to_5);
 
+    /**
+     *
+     * @param seq
+     * @param dir
+     */
     explicit RNA (const Sequence &seq, Directionality dir = Directionality::DIR_3_to_5);
 
+    /**
+     *
+     * @param strand
+     * @return
+     */
     [[nodiscard]] DNA reverseTranscribe(TranscriptionStatus strand = TranscriptionStatus::CODING) const;
 
+    /**
+     *
+     * @return
+     */
     [[nodiscard]] Protein translate() const;
 };
--- tests/testRNA.cpp
+++ tests/testRNA.cpp
@@ -11,167 +11,39 @@
 
 using namespace ::testing;
 
+TEST(TestRNA, TestConstructors) {
+    const std::string s1 = "ACACACUUUGGG";
+    const std::string s2 = "ACACACUUUGGG";
 
-//TEST(TestRNA, TestTrueConstructors) {
-//    std::string s1 = "ACGT";
-//    std::string s2 = "AAAA";
-//    std::string s3 = "GTGT";
-//
-//    DNA dna1{s1, Directionality::DIR_5_to_3};
-//    DNA dna2{s2, Directionality::DIR_3_to_5};
-//    DNA dna3{s3};
-//
-//    ASSERT_EQ(s1, dna1.getSequence());
-//    ASSERT_EQ(s2, dna2.getSequence());
-//    ASSERT_EQ(s3, dna3.getSequence());
-//
-//    ASSERT_EQ(Directionality::DIR_5_to_3, dna1.getDirectionality());
-//    ASSERT_EQ(Directionality::DIR_3_to_5, dna2.getDirectionality());
-//    ASSERT_EQ(ReplicationStatus::LEADING, dna1.getReplicationStatus());
-//    ASSERT_EQ(TranscriptionStatus::TEMPLATE, dna2.getTranscriptionStatus());
-//}
-//
-//TEST(TestRNA, TestFalseConstructors) {
-//    std::string exceptionMessage1;
-//    std::string exceptionMessage2;
-//
-//    const std::string s1 = "ACxT";
-//    const std::string s2 = "";
-//
-//    try {
-//        DNA dna1{s1};
-//    } catch (std::invalid_argument &ex) {
-//        exceptionMessage1 = ex.what();
-//    }
-//    ASSERT_EQ(exceptionMessage1, "SimpleValidator's check of seq is failed on string: \"ACGT\"");
-//
-//    try {
-//        DNA dna2{s2};
-//    } catch (std::invalid_argument &ex) {
-//        exceptionMessage2 = ex.what();
-//    }
-//    ASSERT_EQ(exceptionMessage2, SIMPLE_VALIDATOR_WRONG_SEQUENCE_MESSAGE + " \"ACGT\"");
-//}
-//
-//TEST(TestRNA, TestCopyConstructors) {
-//    std::string s1 = "ACGT";
-//    DNA dna1{s1, Directionality::DIR_5_to_3};
-//    ASSERT_EQ(s1, dna1.getSequence());
-//
-//    DNA dna3{dna1};
-//    ASSERT_EQ(s1, dna3.getSequence());
-//}
-//
-//TEST(TestRNA, TestCopyOperators) {
-//    std::string s1 = "ACGT";
-//    std::string s2 = "AAAA";
-//
-//    DNA dna1{s1, Directionality::DIR_5_to_3};
-//    DNA dna2{s2, Directionality::DIR_3_to_5};
-//
-//    ASSERT_EQ(s1, dna1.getSequence());
-//    ASSERT_EQ(s2, dna2.getSequence());
-//
-//    dna1 = dna2;
-//    ASSERT_EQ(s2, dna1.getSequence());
-//    ASSERT_EQ(Directionality::DIR_3_to_5, dna1.getDirectionality());
-//    dna2 = dna2;
-//    ASSERT_EQ(s2, dna2.getSequence());
-//}
-//
-//TEST(TestRNA, TestMoveConstructors) {
-//    std::string s1 = "ACGT";
-//
-//    DNA dna1{s1, Directionality::DIR_5_to_3};
-//    ASSERT_EQ(s1, dna1.getSequence());
-//
-//    DNA dna3{std::move(dna1)};
-//    ASSERT_EQ(s1, dna3.getSequence());
-//    ASSERT_EQ(Directionality::DIR_5_to_3, dna3.getDirectionality());
-//
-//    ASSERT_TRUE(dna1.getSequence().empty());
-//}
-//
-//TEST(TestRNA, TestMoveOperators) {
-//    std::string s1 = "ACGT";
-//    std::string s2 = "AAAA";
-//
-//    DNA dna1{s1};
-//    DNA dna2{s2};
-//
-//    ASSERT_EQ(s1, dna1.getSequence());
-//    ASSERT_EQ(s2, dna2.getSequence());
-//
-//    dna1 = std::move(dna2);
-//    ASSERT_EQ(s2, dna1.getSequence());
-//    dna1 = std::move(dna1);
-//    ASSERT_EQ(s2, dna1.getSequence());
-//}
-//
-//TEST(TestRNA, TestReverseDNA) {
-//    std::string s1 = "ACGT";
-//    DNA dna1{s1, Directionality::DIR_5_to_3};
-//    ASSERT_EQ(s1, dna1.getSequence());
-//    ASSERT_EQ(Directionality::DIR_5_to_3, dna1.getDirectionality());
-//
-//    dna1.reverse();
-//
-//    ASSERT_EQ("TGCA", dna1.getSequence());
-//    ASSERT_EQ(Directionality::DIR_3_to_5, dna1.getDirectionality());
-//}
-//
-//TEST(TestRNA, TestComplement) {
-//    std::string s1 = "CGTATGCTA";
-//    std::string s2 = "GCATACGAT";
-//    DNA dna1{s1, Directionality::DIR_3_to_5};
-//
-//    ASSERT_EQ(s1, dna1.getSequence());
-//
-//    dna1.complement();
-//    ASSERT_EQ(s2, dna1.getSequence());
-//    ASSERT_EQ(Directionality::DIR_5_to_3, dna1.getDirectionality());
-//
-//    dna1.complement();
-//    ASSERT_EQ(s1, dna1.getSequence());
-//    ASSERT_EQ(Directionality::DIR_3_to_5, dna1.getDirectionality());
-//}
-//
-//TEST(TestRNA, TestReverseComplement) {
-//    std::string s1 = "CGTATGCTA";
-//    std::string s2 = "TAGCATACG";
-//    DNA dna1{s1, Directionality::DIR_3_to_5};
-//
-//    ASSERT_EQ(s1, dna1.getSequence());
-//
-//    dna1.reverseComplement();
-//    ASSERT_EQ(s2, dna1.getSequence());
-//    ASSERT_EQ(Directionality::DIR_3_to_5, dna1.getDirectionality());
-//
-//    dna1.reverseComplement();
-//    ASSERT_EQ(s1, dna1.getSequence());
-//    ASSERT_EQ(Directionality::DIR_3_to_5, dna1.getDirectionality());
-//}
-//
-//TEST(TestRNA, IdempotenceOfComplements) {
-//    //complement + reverse complement + reverse = 2x complement + 2x reverse = doing nothing
-//
-//    std::string s1 = "CGTATGCTA";
-//
-//    DNA dna1{s1, Directionality::DIR_3_to_5};
-//    ASSERT_EQ(s1, dna1.getSequence());
-//    dna1.complement();
-//    dna1.reverseComplement();
-//    dna1.reverse();
-//
-//    ASSERT_EQ(s1, dna1.getSequence());
-//    ASSERT_EQ(Directionality::DIR_3_to_5, dna1.getDirectionality());
-//}
-//
+    const RNA rna1(s1, Directionality::DIR_3_to_5);
+    const RNA rna2{s2, Directionality::DIR_3_to_5};
+
+    ASSERT_EQ(rna1.getSequence(), s1);
+    ASSERT_EQ(rna2.getSequence(), s2);
+    ASSERT_EQ(rna1.getDirectionality(), Directionality::DIR_3_to_5);
+    ASSERT_EQ(rna2.getDirectionality(), Directionality::DIR_3_to_5);
+
+    try {
+        RNA rna3{"ACfsgdjksdhjoi;njkx;dvsvbjkcsvsdbjKsvbjlkx", Directionality::DIR_5_to_3};
+    }
+    catch (std::invalid_argument &e) {
+        ASSERT_EQ("SimpleValidator's check of seq is failed on string: \"ACGU\"", std::string(e.what()));
+    }
+
+    try {
+        RNA rna4{Sequence("ACfsgdjksdhjoi;njkx;dvsvbjkcsvsdbjKsvbjlkx"), Directionality::DIR_5_to_3};
+    }
+    catch (std::invalid_argument &e) {
+        ASSERT_EQ("SimpleValidator's check of seq is failed on string: \"ACGU\"", std::string(e.what()));
+    }
+}
+
+
 TEST(TestRNA, TestReverseTranscription) {
     const std::string s1 = "ACACACUUUGGG";
     const std::string s2 = "UGUGUGAAACCC";
     const RNA rna1(s1, Directionality::DIR_3_to_5);
-    const RNA rna2{s2, Directionality::DIR_5_to_3};
+    const RNA rna2{Sequence(s2), Directionality::DIR_5_to_3};
 
     auto dna1 = rna1.reverseTranscribe(TranscriptionStatus::CODING);
     auto dna2 = rna2.reverseTranscribe(TranscriptionStatus::TEMPLATE);
